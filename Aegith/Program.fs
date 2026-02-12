3e8cd521c743f1e82a9f04418337052693546189
muedever <n-makoto0104@outlook.jp>
2026-02-12 18:38:23 +0900

    _

---
+++ Aegith/Program.fs
@@ -0,0 +1,19 @@
+﻿open Aegith.Compiler
+
+[<EntryPoint>]
+let main arv =
+    let p = Parser()
+    let input = @"
+package main
+import std::fmt
+
+struct A {
+    init new() {@}
+}
+"
+    p.run input |> printfn "%A"
+    let fast = p.getFlatAST()
+    //fast.initData()
+    fast |> printfn "%A"
+    //let hmti = HMTypeInference(fast)
+    0
